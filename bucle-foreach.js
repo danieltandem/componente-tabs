2bd9b9d769174c6234d654a9e2506921f3fdbdea
danieltandem <tandem.danielvargas@gmail.es>
2023-10-31 13:17:34 +0100

    primer commit

---
+++ bucle-foreach.js
@@ -0,0 +1,8 @@
+const personas=['Killian','Dani','Nika'];
+personas.forEach(function(persona,index){
+    console.log(index);
+    console.log(persona);
+});
+personas.forEach((persona,index)=>{
+    console.log(persona)
+});
